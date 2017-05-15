b738c8bbafa1f48e221f2480f44c6c634681a00c
Vítor Luís <vitorluis.mauricio@hotmail.com>
2017-05-15 02:54:08 -0300

    Main Menu
    - Title Screen with Main Menu animation

---
+++ Assets/Scripts/Controller Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	[SerializeField]
+	GameObject menuPanel;
+	[SerializeField]
+	Animator menuAnimator;
+
+	public void ShowMenu() {
+		menuPanel.SetActive (true);
+		menuAnimator.Play ("SlideIn");
+	}
+
+	public void OnPressPlay() {
+		SceneManager.LoadScene ("Gameplay");
+	}
+
+	public void OnPressOptions() {
+		SceneManager.LoadScene ("Options");
+	}
+
+	public void OnPressQuit() {
+		Application.Quit ();
+	}
+}
